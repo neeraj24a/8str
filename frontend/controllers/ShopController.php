dab64fca2a7291fad874990374995e0e225d176d
neeraj24a <neeraj24a@gmail.com>
2019-03-04 15:15:24 +0530

    Gallery Images Added

--- frontend/controllers/ShopController.php
+++ frontend/controllers/ShopController.php
@@ -5,6 +5,7 @@ namespace frontend\controllers;
 use Yii;
 use yii\web\Controller;
 use backend\models\Products;
+use backend\models\ProductGallery;
 use backend\models\Category;
 use backend\models\Drops;
 use frontend\models\AddToCartForm;
@@ -35,24 +36,15 @@ class ShopController extends Controller {
     }
 
     public function actionDetail($product) {
-        /*$info = \backend\models\Products::findOne(['slug' => $product]);
-        $info->main_image = str_replace('../', Yii::$app->homeUrl, $info->main_image);
-        $arr = [];
-        $arr['slug'] = $info->slug;
-        $arr['name'] = $info->name;
-        $arr['unit_price'] = $info->unit_price;
-        $arr['main_image'] = $info->main_image;
-        $arr['description'] = $info->description;
-        $arr['sku'] = $info->sku;
-        $arr['offer_price'] = $info->offer_price;
-        $arr['variation'] = $info->variation;
-        $arr['size'] = $info->size;
-        $arr['colors'] = $info->colors;
-        $arr['available'] = $info->available;
-        $arr['category'] = \backend\models\Category::findOne($info->category)->category;
-
-        echo json_encode($arr);*/
         $info = \backend\models\Products::findOne(['slug' => $product]);
+        $gallery = ProductGallery::find(['product' => $info->id])->all();
+        $info->gallery = [];
+        if($gallery !== null){
+            foreach($gallery as $g){
+                $g->image = str_replace('../', Yii::$app->homeUrl, $g->image);
+                array_push($info->gallery, $g->image);
+            }
+        }
         $info->main_image = str_replace('../', Yii::$app->homeUrl, $info->main_image);
         $info->category = \backend\models\Category::findOne($info->category)->category;
         $addToCart = new AddToCartForm();
